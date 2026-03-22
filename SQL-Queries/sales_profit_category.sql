c7817a8c1f644f36e1da6498cb105a6038ba4b60
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 13:44:52 +0800

    Update sales_profit_category.sql

--- SQL-Queries/sales_profit_category.sql
+++ SQL-Queries/sales_profit_category.sql
@@ -1,6 +1,18 @@
-SELECT "Category",
-   round(SUM("Profit")::numeric, 2) AS "Total Profit"
-FROM products
-GROUP BY "Category"
-ORDER BY "Total Profit" ASC;
+-- Query: Total Profit by Category
+-- Author: John Butch Gromontil
+-- Date: 2026-03-2021
 
+/* 
+
+-- Retrieves the total profit per product category.
+-- Rounded to decimal places and sorted
+-- from lowest to highest proft.
+-- table: products / test (table name) i separate some group but highly suggested to query inside whole csv 
+
+*/
+
+SELECT "Category",
+    ROUND(SUM("Profit")::numeric, 2) AS "Total Profit"  -- rounded to 2 decimal places
+FROM products
+GROUP BY "Category"                                      -- one row per category
+ORDER BY "Total Profit" ASC;                            -- lowest profit first
