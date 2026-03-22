44359b4a562df8ebebcbd0bf8057edcf9cf7f2c1
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 15:49:36 +0800

    Update region_state_analysis.sql

--- SQL-Queries/region_state_analysis.sql
+++ SQL-Queries/region_state_analysis.sql
@@ -1,1 +1,23 @@
+-- Regional and State Performance Overview
+-- Author: John Butch Gromontil
+-- Date: 2026-03-21
 
+/* 
+-- region
+
+
+*/
+
+
+
+
+
+SELECT
+    "Region",
+    "State",
+    ROUND(SUM("Sales")::NUMERIC, 2)                                        AS "Total Sales",
+    ROUND(SUM("Profit")::NUMERIC, 2)                                       AS "Total Profit",
+    ROUND(SUM("Profit")::NUMERIC / SUM("Sales")::NUMERIC * 100, 2)        AS "Profit Margin %"
+FROM test
+GROUP BY "Region", "State"
+ORDER BY "Region" ASC, "Total Sales" DESC;
