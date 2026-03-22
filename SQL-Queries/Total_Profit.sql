96f66fe7c2fac511c4546b1ed2f13ba2d7a7b89e
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 14:10:19 +0800

    Update Total_Profit.sql

--- SQL-Queries/Total_Profit.sql
+++ SQL-Queries/Total_Profit.sql
@@ -1,1 +1,18 @@
+-- Total Profits 
+-- Author: John Butch Gromontil
+-- Date: 2026-03-21
 
+
+/*
+-- Retrievel Total Profit
+-- 
+
+
+*/
+
+
+
+
+
+select ROUND(SUM("Profit"):: numeric,0) as "Total Profit"
+from test;
