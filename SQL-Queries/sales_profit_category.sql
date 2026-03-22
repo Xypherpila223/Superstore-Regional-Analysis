59d030e6ff6b8589ac79fa8057a09991eeaadfcb
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 13:56:16 +0800

    Create sales_profit_category.sql

---
+++ SQL-Queries/sales_profit_category.sql
@@ -0,0 +1,20 @@
+-- Query: Sales & Profit by category
+-- Author: John Butch Gromontil
+-- Date: 2026-03-21
+
+/* 
+
+--
+
+
+
+*/
+
+
+
+
+select "Category", round(SUM("Profit")::numeric, 2) AS "Total Profit",
+round(SUM("Sales")::numeric, 2) AS "Total Sales"
+from products 
+group by "Category"
+order by "Total Profit", "Total Sales";
