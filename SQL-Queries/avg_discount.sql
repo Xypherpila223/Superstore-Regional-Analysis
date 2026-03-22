79f8c1bbd02d2188feb2c3433cebf2887c1252b0
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 14:42:50 +0800

    Update avg_discount.sql

--- SQL-Queries/avg_discount.sql
+++ SQL-Queries/avg_discount.sql
@@ -1,12 +1,15 @@
--- Overall discount overview
+-- KPI total average discount overview
 -- AUthor: John Butch Gromontil 
 -- Date: 2026-03-21
 
 
 /*
--- 
-
+-- Overall average discount
+-- round to 1 decimal place
+-- Table: Products / test (table_name)
+-- Used for KPI Dashboard Card
 */
+
 SELECT 
    ROUND(AVG("Discount")::NUMERIC * 100, 1) AS "Average Discount %"
 FROM products;
