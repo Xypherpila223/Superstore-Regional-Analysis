9e3283008af7e4b245fe4a04e6473ca2b2ac9716
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 14:13:44 +0800

    Update Total_sales.sql

--- SQL-Queries/Total_sales.sql
+++ SQL-Queries/Total_sales.sql
@@ -1,1 +1,15 @@
+-- Total Sales
+-- Author: John Butch Gromontil
+-- Date: 2026-03-21
 
+
+/*
+-- Retrievel Total Sales
+-- Round to 0 decimal places]
+-- Table: Products / Test (table_name)
+-- Used as KPI Card
+*/
+
+
+select ROUND(SUM("Sales"):: numeric,0) as "Total Sales"
+from test;
