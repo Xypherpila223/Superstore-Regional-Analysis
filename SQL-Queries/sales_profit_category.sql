7d9caeecd722dd40e6cbb533b508c4b9c53832fd
John Butch Gromontil <143048044+Xypherpila223@users.noreply.github.com>
2026-03-22 14:02:50 +0800

    Update sales_profit_category.sql

--- SQL-Queries/sales_profit_category.sql
+++ SQL-Queries/sales_profit_category.sql
@@ -5,9 +5,10 @@
 /* 
 
 -- Retrieves Total Sales & Profit by Category
---
-
-
+-- Rounded to decimal and sorted by profit and sales
+-- lowest to Highest profit and sales
+-- Table: Products / Test (table_name)
+-- this query used as my metric for dashboard
 
 */
 
